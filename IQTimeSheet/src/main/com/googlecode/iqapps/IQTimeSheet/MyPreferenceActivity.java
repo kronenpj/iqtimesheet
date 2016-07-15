8c6846148c2add12373557e3b990af468e62b487
Paul Kronenwetter <kronenpj@gmail.com>
2016-07-14 22:29:24 -0400

    Preferences work again, though with an extra page to go through.

--- IQTimeSheet/src/main/com/googlecode/iqapps/IQTimeSheet/MyPreferenceActivity.java
+++ IQTimeSheet/src/main/com/googlecode/iqapps/IQTimeSheet/MyPreferenceActivity.java
@@ -17,106 +17,50 @@
 /**
  * @author (classes and interfaces only, required)
  * @version (classes and interfaces only, required. See footnote 1)
- * @param       (methods and constructors only)
+ * @param (methods and constructors only)
  * @return (methods only)
  * @exception (@throws is a synonym added in Javadoc 1.2)
- * @see
  * @ deprecated  (see How and When To Deprecate APIs)
+ * @see
  */
 package com.googlecode.iqapps.IQTimeSheet;
 
-import java.util.Calendar;
-import java.util.TimeZone;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
-import android.util.Log;
+import android.preference.PreferenceFragment;
 
-import android.view.MenuItem;
+import java.util.List;
+import java.util.TimeZone;
 
 /**
- * @author kronenpj Borrowed heavily from ConnectBot's SettingsActivity.
+ * @author kronenpj
  */
-public class MyPreferenceActivity extends PreferenceActivity implements
-		SharedPreferences.OnSharedPreferenceChangeListener {
-	private static final String TAG = "MyPreferenceActivity";
+public class MyPreferenceActivity extends PreferenceActivity {
+    private static final String TAG = "MyPreferenceActivity";
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+    //	@Override
+    //	public void onCreate(Bundle savedInstanceState) {
+    //		super.onCreate(savedInstanceState);
+    //
+    //		// Load the preferences from an XML resource
+    //		addPreferencesFromResource(R.xml.preferences);
+    //
+    //		ListPreference tzAnchor = (ListPreference) findPreference("tz.anchor");
+    //		tzAnchor.setOrder(70);
+    //		String[] timeZones = TimeZone.getAvailableIDs();
+    //		tzAnchor.setEntries(timeZones);
+    //		tzAnchor.setEntryValues(timeZones);
+    //    }
 
-		/*
-		 * try { addPreferencesFromResource(R.xml.preferences); } catch
-		 * (Exception e) { // Something bad happened when reading the
-		 * preferences. Try to // recover. SharedPreferences prefs =
-		 * PreferenceManager .getDefaultSharedPreferences(this);
-		 * 
-		 * Editor prefEditor = prefs.edit(); // Make sure we're starting from
-		 * scratch prefEditor.clear(); prefEditor.commit();
-		 * 
-		 * // This apparently needs a new editor created...
-		 * PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
-		 * prefEditor = prefs.edit(); prefEditor.commit();
-		 * 
-		 * // Try loading the preferences again.
-		 * addPreferencesFromResource(R.xml.preferences); }
-		 */
-
-		//getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		//getActionBar().setDisplayHomeAsUpEnabled(true);
-		getPreferenceScreen().getSharedPreferences()
-				.registerOnSharedPreferenceChangeListener(this);
-	}
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-		addPreferencesFromResource(R.xml.preferences);
-
-		// SharedPreferences sp = getPreferenceScreen().getSharedPreferences();
-
-		ListPreference tzAnchor = (ListPreference) findPreference("tz.anchor");
-		tzAnchor.setOrder(70);
-		String[] timeZones = TimeZone.getAvailableIDs();
-		tzAnchor.setEntries(timeZones);
-		tzAnchor.setEntryValues(timeZones);
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.headers_preference, target);
     }
 
-	@Override
-	protected void onPause() {
-		getPreferenceScreen().getSharedPreferences()
-				.unregisterOnSharedPreferenceChangeListener(this);
-		finish();
-		super.onPause();
-	}
-
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
-		/*
-		 * Preference pref = findPreference(key); if (pref instanceof
-		 * EditTextPreference) { EditTextPreference etp = (EditTextPreference)
-		 * pref; if (pref.getKey().equals("password")) {
-		 * pref.setSummary(etp.getText().replaceAll(".", "*")); } else {
-		 * pref.setSummary(etp.getText()); } } else if (pref instanceof
-		 * CheckBoxPreference) { if (((CheckBoxPreference) pref).isChecked())
-		 * pref.setSummary("On"); else pref.setSummary("Off"); }
-		 */
-	}
-
-	public boolean onOptionsItemSelected(MenuItem menuItem) {
-		switch (menuItem.getItemId()) {
-		case android.R.id.home: {
-			finish();
-			return true;
-		}
-		default:
-			return super
-					.onOptionsItemSelected((android.view.MenuItem) menuItem);
-		}
-	}
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return MyPreferenceActivity.class.getName().equals(fragmentName) ||
+                MyPreferenceFragment.class.getName().equals(fragmentName);
+    }
 }
